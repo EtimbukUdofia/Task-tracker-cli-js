82ae1d303539c18499bb90fd0c2eead1b3bd4cc7
EtimbukUdofia <etimbukudofia9@gmail.com>
2025-03-17 21:35:57 +0100

    test: update tests to handle the deletion of all tasks

--- test/deleteTask.test.js
+++ test/deleteTask.test.js
@@ -9,6 +9,29 @@ describe("deleteTask function", () => {
     console.log = jest.fn();
   });
 
+  test('should delete all tasks', () => { 
+    const mockTasks = [
+      { id: 1, description: "Task 1", status: "todo" },
+      { id: 2, description: "Task 2", status: "done" },
+    ];
+
+    fs.existsSync.mockReturnValue(true);
+    fs.readFileSync.mockImplementation(() => JSON.stringify(mockTasks));
+
+    deleteTask("all");
+
+    expect(fs.writeFileSync).toHaveBeenCalledWith(expect.any(String), JSON.stringify([]));
+    expect(console.log).toHaveBeenCalledWith("All tasks deleted successfully");
+  });
+  
+  test('should throw an error if trying to delete all tasks but no task in task file', () => {
+    fs.existsSync.mockReturnValue(true);
+    fs.readFileSync.mockImplementation(() => JSON.stringify([]));
+
+    expect(() => deleteTask("all")).toThrow("No task currently in memory");
+    expect(console.log).toHaveBeenCalledWith(expect.any(Error));
+  });
+
   test("should delete a task given it's ID", () => {
     const mockTasks = [
       { id: 1, description: "Task 1", status: "todo" },
